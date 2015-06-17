e92bd59a179fe0fc731b864007cb9f28d3fe7077
Maxi <maxidetorres@gmail.com>
2015-06-17 17:19:37 -0300

    a

--- LigaDeFutbol/LigaDeFutbolDAL/DAL/FixtureDAL.cs
+++ LigaDeFutbol/LigaDeFutbolDAL/DAL/FixtureDAL.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LigaDeFutbolDTO;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace LigaDeFutbolDAL.DAL
+namespace LigaDeFutbolDAL
 {
     public class FixtureDAL
     {
-
+        public static void insertar(,)
     }
 }
