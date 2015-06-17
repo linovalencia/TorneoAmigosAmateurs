c57b1cffef7db1516c59e4258678d76951b5273a
marciobagnus <marcyob@gmail.com>
2015-06-17 16:57:45 -0300

    c

---
+++ LigaDeFutbol/LigaDeFutbolDAL/DAL/JugadorDAL.cs
@@ -0,0 +1,31 @@
+﻿using LigaDeFutbolDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LigaDeFutbolDAL.DAL
+{
+    public class JugadorDAL
+    {
+        internal static void Insertar(JugadorDTO jugador, SqlConnection cn, SqlTransaction tran)
+        {
+            string consultaSql = @"INSERT INTO jugador(idTipoDocumento,numeroDocumento,nombre,apellido,fechaNacimiento)
+                                   VALUES(@idTipo,@nroDoc,@nomJug,@apeJug,@fechaNac)";
+
+            SqlCommand cmd = new SqlCommand(consultaSql, cn, tran);
+
+            cmd.Parameters.AddWithValue("@idTipo", jugador.idTipoDocumento);
+            cmd.Parameters.AddWithValue("@nroDoc", jugador.numeroDocumento);
+            cmd.Parameters.AddWithValue("@nomJug", jugador.nombre);
+            cmd.Parameters.AddWithValue("@apeJug", jugador.apellido);
+            cmd.Parameters.AddWithValue("@fechaNac", jugador.fechaNacimiento);
+            
+
+            jugador.idInscripcion = Convert.ToInt32(cmd.ExecuteScalar());
+        }
+    }
+}
---
+++ LigaDeFutbol/LigaDeFutbolDTO/Entidad/FixtureDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaDeFutbolDTO.Entidad
+{
+    public class FixtureDTO:DTOBase
+    {
+        public int numeroFecha { get; set; }
+        public int idCampeonato { get; set; }
+        public DateTime fechaComienzo { get; set; }
+        public int idEstadoFecha { get; set; }
+        
+        public FixtureDTO()
+        {
+            numeroFecha = int_NullValue;
+            idCampeonato = int_NullValue;
+            fechaComienzo = dateTime_NullValue;
+            idEstadoFecha = int_NullValue;
+            IsNew = true;
+        }
+
+    }
+}
