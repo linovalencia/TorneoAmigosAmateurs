c7695407649439a31cec448ce8f07ae062623c3a
Maxi <maxidetorres@gmail.com>
2015-06-17 16:29:12 -0300

    se agrego CampeonatoDAL,FixtureDAL,EstadoFecha,FixtureDTO

---
+++ LigaDeFutbol/LigaDeFutbolDAL/DAL/CampeonatoDAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LigaDeFutbolDTO;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LigaDeFutbolDAL
+{
+    public class CampeonatoDAL
+    {
+        public static List<CampeonatoDTO> obtenerCampeonatos()
+        {
+            List<CampeonatoDTO> campeonatos = new List<CampeonatoDTO>();
+            string consultaSQL = "SELECT * FROM campeonato";
+            SqlConnection cnn = new SqlConnection(DALBase.StringConexion);
+
+            try
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand(consultaSQL, cnn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    CampeonatoDTO c = new CampeonatoDTO();
+                    c.idCampeonato = int.Parse(dr["idCampeonato"].ToString());
+                    c.nombre = dr["nombre"].ToString();
+                    c.fechaInicio = DateTime.Parse(dr["fechaInicio"].ToString());
+                    c.fechaFin = DateTime.Parse(dr["fechaFin"].ToString());
+                    c.idEstadoCampeonato = int.Parse(dr["idEstadoCampeonato"].ToString());
+                    c.puntosPartidoGanado = int.Parse(dr["puntosPartidoGanado"].ToString());
+                    c.puntosPartidoEmpatado = int.Parse(dr["puntosPartidoEmpatado"].ToString());
+                    c.fechaLimiteInscripcion = DateTime.Parse(dr["apellido"].ToString());
+                    
+                   
+
+                    campeonatos.Add(c);
+                }
+                cnn.Close();
+            }
+
+
+            catch (SqlException ex)
+            {
+                if (cnn.State == ConnectionState.Open)
+                    cnn.Close();
+                throw new ApplicationException("Error al buscar los campeonatos " + ex.ToString());
+
+            }
+            return campeonatos;
+        }
+    }
+}
---
+++ LigaDeFutbol/LigaDeFutbolDTO/Entidad/EstadoFecha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaDeFutbolDTO
+{
+    public class EstadoFecha : DTOBase
+    {
+        public int idEstadoFecha { get; set; }
+        public string descripcion { get; set; }
+
+
+        public EstadoFecha()
+        {
+            idEstadoFecha = int_NullValue;
+            descripcion = string_NullValue;
+            IsNew = true;
+        }
+    }
+}
